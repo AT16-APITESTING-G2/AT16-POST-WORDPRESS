64b95dd06a0afe1802ff781284cd51667fbd8458
Alvaro Quena <alvaro.quena1@gmail.com>
2022-07-19 19:23:21 -0400

    structure of proyect

---
+++ test/test_create_post.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from assertpy import assert_that
+
+from crud_post import CrudPost
+
+
+class TestCreatePost(TestCase):
+    def test_create_post(self):
+        url = ""
+        token = ""
+        content = ""
+        page = ""
+        status = ""
+        title = ""
+
+        crud_post = CrudPost()
+        crud_post.create_post(url, token, title, content, page, status)
+        assert_that()
+
