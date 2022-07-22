1f2020b024f5c7d7c1314cc2baebeec4f9bb52fa
daylerVMT <dayler.vmt7@gmail.com>
2022-07-22 13:05:15 -0400

    add refactor code

--- test/test_delete_post.py
+++ test/test_delete_post.py
@@ -1,3 +1,16 @@
+#
+# @test_create_post.py Copyright (c) 2022 Jalasoft
+# 2643 Av Melchor Perez de Olguin , Colquiri Sud, Cochabamba, Bolivia.
+# add direccion de jala la paz>
+# All rights reserved
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 import http
 import json
 from decouple import config
@@ -9,12 +22,13 @@ from crud_post import CrudPost
 class DeletePost(TestCase):
 
     def test_delete_post(self):
+
+        url = config('URL')
+        id_post = config('ID_POST')
+        token = config('TOKEN')
         crud_post = CrudPost()
-        response = crud_post.delete_post(config('URL'), config('TOKEN'), config('ID_POST'))
+        response = crud_post.delete_post(url, token, id_post)
+        response_str_void = crud_post.delete_post(url, token, " ")
         assert_that(response.status_code).is_equal_to(http.HTTPStatus.OK)
-
-
-
-
-
+        assert_that(response_str_void.status_code).is_equal_to(http.HTTPStatus.NOT_FOUND)
 
